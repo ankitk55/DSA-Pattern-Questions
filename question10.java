017ff1538b527726bc797db04107e04a7a52a655
Ankit kumar <anktikumar55@gmail.com>
2023-08-12 23:22:52 +0530

    question 10 done

---
+++ question10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class question10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=n-i;j>=1;j--){
+                System.out.print("  ");
+            }
+            for(int j=2*i-1;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

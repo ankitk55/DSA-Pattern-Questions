373c8c1a0dc642f9f48162a60afb8ac6c92e6de8
Ankit kumar <anktikumar55@gmail.com>
2023-08-13 12:52:27 +0530

    question 17 done

---
+++ question17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class question17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+        if(i<=n/2+1){
+            for(int j=1;j<=n/2-i+1;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("  ");
+            }
+             for(int j=1;j<=n/2-i+1;j++){
+                System.out.print("* ");
+            }
+        } 
+        else{
+            for(int j=1;j<=(i-n/2)-1;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=(2*(n-i))+1;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(i-n/2)-1;j++){
+                System.out.print("* ");
+            }
+        }
+        System.out.println();
+        }
+    }
+    
+}

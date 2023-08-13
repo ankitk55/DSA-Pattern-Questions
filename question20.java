78e66e0854261967c57ab093bce899a95975e099
Ankit kumar <anktikumar55@gmail.com>
2023-08-13 14:47:36 +0530

    question 20 done

---
+++ question20.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class question20{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i<=n/2+1){
+                for(int j=1;j<=n/2-i+1;j++){
+                    System.out.print("  ");
+                }
+                for(int j =1;j<=1;j++){
+                    System.out.print("* ");
+                }
+                if(i!=1){
+                for(int j=1;j<=2*i-3;j++){
+                    System.out.print("  ");
+                }
+                 for(int j =1;j<=1;j++){
+                    System.out.print("* ");
+                }
+            }
+            }
+            else{
+                for(int j=1;j<=i-n/2-1;j++){
+                    System.out.print("  ");
+                }
+                 for(int j =1;j<=1;j++){
+                    System.out.print("* ");
+                }
+                if(i!=n){
+                for(int j=1;j<=2*(n-i)-1;j++){
+                    System.out.print("  ");
+                }
+                 for(int j =1;j<=1;j++){
+                    System.out.print("* ");
+                }
+            }
+            }
+            System.out.println();
+        }
+    }
+}
